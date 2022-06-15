4a78e3661af605ddaabb3b57ad7225c3687ed7b5
Muhamad Jakfar Shodiq <jakfarsoo@gmail.com>
2022-06-15 21:06:40 +0700

    Initial Commit

---
+++ TMDB Orange/TMDB_OrangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_OrangeApp.swift
+//  TMDB Orange
+//
+//  Created by Administrator  on 15/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_OrangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
